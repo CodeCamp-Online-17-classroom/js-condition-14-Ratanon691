8c5a62bfcf45e648520a92d6911517ae46708869
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-23 00:59:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+let a = +prompt(`a?`)
+let value
+switch(a){
+  case 0:
+  value = 0
+    break
+  case 1:
+  value = 1
+    break
+  case 2 || 3:
+  value = `2,3`
+    break
+}
+alert(`${value}`)
+        
+      
+      
